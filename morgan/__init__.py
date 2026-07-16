1170cf40a64f711b37907ae29c5ca7ac889bed9d
Christian Schmidbauer <christian.schmidbauer@risefx.com>
2026-07-16 16:28:07 +0200

    temporary adjustments to test pypi publishing

--- morgan/__init__.py
+++ morgan/__init__.py
@@ -37,7 +37,7 @@ else:
     from importlib import metadata as _metadata
 
 try:
-    __version__ = _metadata.version("morgan")
+    __version__ = _metadata.version("morgen-tmp")
 except _metadata.PackageNotFoundError:
     # Running from a source tree that hasn't been installed.
     __version__ = "0.0.0+unknown"
